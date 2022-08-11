4cef18f18e10c13f313ae354cb9fbd77b60ca74c
Roland Rabien <figbug@gmail.com>
2022-08-11 11:59:16 -0700

    Added ComboBox::isScrollWheelEnabled()

--- modules/juce_gui_basics/widgets/juce_ComboBox.cpp
+++ modules/juce_gui_basics/widgets/juce_ComboBox.cpp
@@ -615,6 +615,11 @@ void ComboBox::setScrollWheelEnabled (bool enabled) noexcept
     scrollWheelEnabled = enabled;
 }
 
+bool ComboBox::isScrollWheelEnabled() const noexcept
+{
+	return scrollWheelEnabled;
+}
+
 //==============================================================================
 void ComboBox::addListener    (ComboBox::Listener* l)    { listeners.add (l); }
 void ComboBox::removeListener (ComboBox::Listener* l)    { listeners.remove (l); }
