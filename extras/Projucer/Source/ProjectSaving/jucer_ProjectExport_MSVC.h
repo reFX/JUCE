0b5b3914cf26341988912da76cfcad270bd82785
Roland Rabien <figbug@gmail.com>
2025-06-12 14:43:45 -0700

    Revert reFX change

--- extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_MSVC.h
+++ extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_MSVC.h
@@ -2215,11 +2215,10 @@ public:
     {
        #if JUCE_WINDOWS
         // Don't launch if already open
-        if (getSLNFile().getSiblingFile (".vs").findChildFiles (File::findFiles, true, "*.opendb", File::FollowSymlinks::no).size() > 0)
-            return false;
+        const auto foundDBFiles = getSLNFile().getSiblingFile (".vs").findChildFiles (File::findFiles, true, "*.opendb", File::FollowSymlinks::no);
 
-        if (launchViaVSWhere())
-            return true;
+        if (! foundDBFiles.isEmpty())
+            return false;
 
         return getSLNFile().startAsProcess();
        #else
@@ -2333,41 +2332,6 @@ private:
         return getVisualStudioVersion() < 10  // (VS10 automatically adds escape characters to the quotes for this definition)
                                           ? CppTokeniserFunctions::addEscapeChars (rebasedPath.quoted())
                                           : CppTokeniserFunctions::addEscapeChars (rebasedPath).quoted();
-    }
-
-    bool launchViaVSWhere()
-    {
-        auto vswhere = juce::File::getSpecialLocation (juce::File::globalApplicationsDirectoryX86).getChildFile ("Microsoft Visual Studio\\Installer\\vswhere.exe");
-        if (! vswhere.existsAsFile())
-            return false;
-
-        juce::ChildProcess proc;
-        proc.start ({vswhere.getFullPathName(), "-format", "json"});
-        proc.waitForProcessToFinish (1000);
-        
-        auto json = JSON::parse (proc.readAllProcessOutput());
-        if (! json.isArray())
-            return false;
-
-        for (auto ver : *json.getArray())
-        {
-            if (! ver.isObject())
-                continue;
-
-            auto catalog = ver.getProperty ("catalog", {});
-            if (catalog.isObject())
-            {
-                if (catalog.getProperty ("buildVersion", {}).toString().startsWith (juce::String (getVisualStudioVersion())))
-                {
-                    auto vs = File (ver.getProperty ("productPath", {}).toString());
-                    if (vs.existsAsFile())
-                        if (vs.startAsProcess (getSLNFile().getFullPathName().quoted()))
-                            return true;
-                }
-            }
-        }
-
-        return false;
     }
 
 protected:
