reFX/JUCE

commit 8f079a4ae2e9e8da27bb357957d4f208216ea7f5

AudioDeviceManager: Improve sample rate and buffer size selection
When changing sample rate the available buffer sizes may change. In
that case we need to configure the buffer size after changing the
sample rate. This change also improves the case that the exact sample
rate or buffer size isn't available, by selecting the next nearest
value.

modules/juce_audio_devices/audio_io/juce_AudioDeviceManager.cpp
@@ -883,13 +883,28 @@ String AudioDeviceManager::setAudioDeviceSetup (const AudioDeviceSetup& newSetup
         return {};
     }
 
-    currentSetup.sampleRate = chooseBestSampleRate (currentSetup.sampleRate);
-    currentSetup.bufferSize = chooseBestBufferSize (currentSetup.bufferSize);
+    const auto sampleRate = findNearestValue (Span { currentAudioDevice->getAvailableSampleRates() }, currentSetup.sampleRate);
+    const auto preBufferSize = findNearestValue (Span { currentAudioDevice->getAvailableBufferSizes() }, currentSetup.bufferSize);
 
     error = currentAudioDevice->open (currentSetup.inputChannels,
                                       currentSetup.outputChannels,
-                                      currentSetup.sampleRate,
-                                      currentSetup.bufferSize);
+                                      sampleRate,
+                                      preBufferSize);
+
+    if (error.isEmpty())
+    {
+        // The available buffer sizes may have changed, if the sample rate has
+        // changed. If so we'll need to reconfigure with a different buffer size.
+        const auto postBufferSize = findNearestValue (Span { currentAudioDevice->getAvailableBufferSizes() }, currentSetup.bufferSize);
+
+        if (preBufferSize != postBufferSize)
+        {
+            error = currentAudioDevice->open (currentSetup.inputChannels,
+                                              currentSetup.outputChannels,
+                                              currentSetup.sampleRate,
+                                              postBufferSize);
+        }
+    }
 
     if (error.isEmpty())
     {
@@ -917,46 +932,6 @@ String AudioDeviceManager::setAudioDeviceSetup (const AudioDeviceSetup& newSetup
     }
 
     return error;
-}
-
-double AudioDeviceManager::chooseBestSampleRate (double rate) const
-{
-    jassert (currentAudioDevice != nullptr);
-
-    auto rates = currentAudioDevice->getAvailableSampleRates();
-
-    if (rate > 0 && rates.contains (rate))
-        return rate;
-
-    rate = currentAudioDevice->getCurrentSampleRate();
-
-    if (rate > 0 && rates.contains (rate))
-        return rate;
-
-    double lowestAbove44 = 0.0;
-
-    for (int i = rates.size(); --i >= 0;)
-    {
-        auto sr = rates[i];
-
-        if (sr >= 44100.0 && (lowestAbove44 < 1.0 || sr < lowestAbove44))
-            lowestAbove44 = sr;
-    }
-
-    if (lowestAbove44 > 0.0)
-        return lowestAbove44;
-
-    return rates[0];
-}
-
-int AudioDeviceManager::chooseBestBufferSize (int bufferSize) const
-{
-    jassert (currentAudioDevice != nullptr);
-
-    if (bufferSize > 0 && currentAudioDevice->getAvailableBufferSizes().contains (bufferSize))
-        return bufferSize;
-
-    return currentAudioDevice->getDefaultBufferSize();
 }
 
 void AudioDeviceManager::stopDevice()

commit 2af5e5bf198aad772df0a1eb999eea70b44f1d92

Linux: AudioDeviceManager: Fix default sample rate when the device's current sample rate is 0
While the codepath affects all platforms, it was observed causing issues
on Linux. This fixes a regression in
8f079a4ae2e9e8da27bb357957d4f208216ea7f5.

Co-authored-by: Jonathan Feinberg <eecoli@gmail.com>

modules/juce_audio_devices/audio_io/juce_AudioDeviceManager.cpp
@@ -817,6 +817,14 @@ static T getNonZeroOrDefault (T value, T defaultValue)
     return value;
 }
 
+static double getDefaultRequestedSampleRate (AudioIODevice& device)
+{
+    if (const auto current = device.getCurrentSampleRate(); current > 0.0)
+        return current;
+
+    return 44100.0;
+}
+
 String AudioDeviceManager::setAudioDeviceSetup (const AudioDeviceSetup& newSetup,
                                                 bool treatAsChosenDevice)
 {
@@ -893,7 +901,7 @@ String AudioDeviceManager::setAudioDeviceSetup (const AudioDeviceSetup& newSetup
     }
 
     currentSetup.sampleRate = findNearestValue (Span { currentAudioDevice->getAvailableSampleRates() },
-                                                getNonZeroOrDefault (currentSetup.sampleRate, currentAudioDevice->getCurrentSampleRate()));
+                                                getNonZeroOrDefault (currentSetup.sampleRate, getDefaultRequestedSampleRate (*currentAudioDevice)));
 
     const auto requestedBufferSize = getNonZeroOrDefault (currentSetup.bufferSize, currentAudioDevice->getDefaultBufferSize());
     const auto preBufferSize = findNearestValue (Span { currentAudioDevice->getAvailableBufferSizes() }, requestedBufferSize);